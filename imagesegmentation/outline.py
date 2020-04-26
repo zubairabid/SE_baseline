561c6e36a828580ae6743624bab1e2876b6e1f51
zubairabid <zubairabid1999+github@gmail.com>
2020-04-26 06:36:25 +0530

    Extremely rudimentary but overview of how I thought modules will fit

---
+++ imagesegmentation/outline.py
@@ -0,0 +1,9 @@
+def get_segments(path_to_image):
+    # voodoo image segmentation stuff
+    # Extracts Individual answers (paragraphs) from a page. Each paragraph
+    # should have a number attached to it. Generates n images, where n
+    # is the number of answers on the page. Also thresholds etc the images.
+    # Each image is saved as <tmp>/i.png
+
+    list_of_segments = [] # paths to the image segements
+    return list_of_segments
